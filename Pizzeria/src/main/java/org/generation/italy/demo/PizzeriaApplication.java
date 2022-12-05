61fe3ea94ba30b60d4eb7a8044688019a6a89ba7
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-05 17:04:26 +0100

    Update PizzeriaApplication.java
    implemented  CommandLineRunner & added pizza examples in insertion and reading

--- Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
@@ -1,13 +1,36 @@
 package org.generation.italy.demo;
 
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PizzeriaApplication {
+public class PizzeriaApplication implements CommandLineRunner {
 
+	@Autowired
+	private PizzaService pizzaService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Pizza pizza_1 = new Pizza("Margherita", "Troppo buona", 7);
+		Pizza pizza_2 = new Pizza("Marinara", "Ci sta, bel sapore", 6);
+		Pizza pizza_3 = new Pizza("Fritta", "Croccante e buona frittura", 8);
+		
+		pizzaService.save(pizza_1);
+		pizzaService.save(pizza_2);
+		pizzaService.save(pizza_3);
+		
+		List<Pizza> pizzas = pizzaService.findAll();
+		System.out.println(pizzas);
+	}
 }
