035d404ed4e31209b916aa1853a806c38c1724d0
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-06 15:33:12 +0100

    Update PizzeriaApplication.java
    added drinks examples in insertion and reading

--- Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
@@ -2,7 +2,9 @@ package org.generation.italy.demo;
 
 import java.util.List;
 
+import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.service.DrinkService;
 import org.generation.italy.demo.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,9 @@ public class PizzeriaApplication implements CommandLineRunner {
 
 	@Autowired
 	private PizzaService pizzaService;
+	
+	@Autowired
+	private DrinkService drinkService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaApplication.class, args);
@@ -32,5 +37,18 @@ public class PizzeriaApplication implements CommandLineRunner {
 		
 		List<Pizza> pizzas = pizzaService.findAll();
 		System.out.println(pizzas);
+		
+		// drinks
+		
+		Drink drink_1 = new Drink("Birra chiara", "Leggera e buona", 5);
+		Drink drink_2 = new Drink("Birra scura", "Sapore corposo", 5);
+		Drink drink_3 = new Drink("Soft drink", "Coca cola o Fanta", 3);
+		
+		drinkService.save(drink_1);
+		drinkService.save(drink_2);
+		drinkService.save(drink_3);
+		
+		List<Drink> drinks = drinkService.findAll();
+		System.out.println(drinks);
 	}
 }
