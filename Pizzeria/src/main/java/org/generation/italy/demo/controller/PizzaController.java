e242705fdb96d4d746bc2d646b558e49f5807c96
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-07 14:51:54 +0100

    feat(PizzaController): add GetMapping to search by name
    management of search and input parameters passing through the service
    
    - if the query is found it returns the result, otherwise all the elements

--- Pizzeria/src/main/java/org/generation/italy/demo/controller/PizzaController.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/controller/PizzaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
@@ -105,5 +106,29 @@ public class PizzaController {
 		pizzaService.delete(pizza);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/search")
+	public String getSearchPizzaByName(Model model, 
+			@RequestParam(name = "q", required = false) String query) {
+
+//		List<Pizza> pizzas = null;
+//		if (query == null) {
+//			
+//			pizzas = pizzaService.findAll();
+//			
+//		} else {
+//			
+//			pizzas = pizzaService.findByName(query);
+//		}
+		
+		List<Pizza> pizzas = query == null 
+							? pizzaService.findAll()
+							: pizzaService.findByNome(query); 
+
+		model.addAttribute("pizze", pizzas);
+		model.addAttribute("query", query);
+
+		return "pizze-search";
 	}
 }
